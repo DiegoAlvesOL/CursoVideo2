712748f2b4f779be1c5177925ffe11dd00c19cd1
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-03-27 22:25:06 +0000

    general files

---
+++ class/calssDiego.py
@@ -0,0 +1,9 @@
+size = int(input("Digite a quantidade de repetições: "))
+num = int(input("Digite um número para começar a somar: "))
+
+for i in range(0, size):
+    num = num + 1
+    print("o resultado da soma  é {}".format(num))
+
+
+
